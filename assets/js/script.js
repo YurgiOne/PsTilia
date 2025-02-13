f11f1efa0331ae27c535d75626384a594032c8e4
Nikola Jurgec <njurgec@hotmail.com>
2025-02-13 10:17:35 +0100

    proto

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 imeTvrtke: "Poslovni Sistemi Tilia d.o.o.",
                 sustaviNaplatedescription:"Jedinstveni sustavi naplate",
                 opisTvrtke:"Napredni OCR sustav za naplatu i kontrolu parkiranja. Automatiziramo prepoznavanje registarskih oznaka, praćenje ulaska i izlaska vozila, naplatu te automatsko izdavanje dnevnih parkirnih karata, omogućujući precizan nadzor parkirališta u stvarnom vremenu.",
+                opisTiliaParagraph2:"Naš sustav implementiran je u više gradova i naselja diljem Hrvatske, uključujući javne garaže i parking zone u Zagrebu, Zadru i Dubrovniku, a sudjelujemo i u razvoju centralnog sustava za nadzor i regulaciju prometa Grada Zagreba.",
                 sekcijaDvaNaslov:"Ključne Prednosti",
                 description: "Ovo je višejezična stranica.",
                 naviHomedescription: "NASLOVNICA",
@@ -14,6 +15,8 @@ document.addEventListener("DOMContentLoaded", () => {
         en: { translation: {
                 imeTvrtke: "PS Tilia",
                 description: "This is a multilingual website.",
+                opisTvrtke:"Advanced OCR system for parking payment and control. We automate license plate recognition, vehicle entry and exit tracking, payment processing, and automatic issuance of daily parking tickets, enabling precise real-time parking lot monitoring.",
+                opisTiliaParagraph2: "Our system has been implemented in multiple cities and towns across Croatia, including public garages and parking zones in Zagreb, Zadar, and Dubrovnik. We are also involved in developing the central traffic monitoring and regulation system for the City of Zagreb",
                 sustaviNaplatedescription:" ",
                 naviHomedescription: "HOME",
                 naviSectionOnedescription: "SECTION ONE",
